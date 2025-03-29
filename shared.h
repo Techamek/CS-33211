fc3935f8c39f87553e326165d096baed869d0778
Techamek <70824883+Techamek@users.noreply.github.com>
2025-03-28 20:53:11 -0400

    Update shared.h

--- shared.h
+++ shared.h
@@ -1,1 +1,27 @@
+#ifndef SHARED_H_
+#define SHARED_H_
 
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+static const int bufferSize = 2;
+
+struct table {
+    int buffer[bufferSize];
+    int in;
+    int out;
+};
+
+static const int SIZE = sizeof(struct table);
+static const char *NAME = "ProdConsumnProb";
+static const char *semFullName = "full";
+static const char *semEmptyName = "empty";
+static const char *semMutexName = "mutex";
+
+#endif //SHARED_H_
