e5c62dad587d671876876b7eea183129a288a928
Techamek <70824883+Techamek@users.noreply.github.com>
2025-03-28 21:56:32 -0400

    Update producer.c

--- producer.c
+++ producer.c
@@ -2,11 +2,12 @@
 
 int main() {
     int fd = shm_open(NAME, O_CREAT | O_RDWR, 0666);
-    ftruncate(fd, SIZE);
 
+    ftruncate(fd, SIZE);
+    
     struct table *share = mmap(0, SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
-    if (share == MAP_FAILED) {
-        perror("Error: Shared memory mapping failed");
+    if(share == MAP_FAILED){
+        perror("Error: Failed to map shared memory");
         return EXIT_FAILURE;
     }
 
@@ -16,35 +17,35 @@ int main() {
     sem_t *full = sem_open(semFullName, O_CREAT, 0666, 0);
     sem_t *empty = sem_open(semEmptyName, O_CREAT, 0666, bufferSize);
     sem_t *mutex = sem_open(semMutexName, O_CREAT, 0666, 1);
-
-    srand((unsigned) time(NULL));
+    
+    time_t t;
+    srand((unsigned) time(&t));
+    
     int loop = 10;
 
     printf("\n[Producer] Process has started\n");
 
-    while (loop--) {
+    while(loop--) {
         sem_wait(empty);
-        sleep(rand() % 3);
-
+        sleep(rand()%3);
         sem_wait(mutex);
-        sleep(rand() % 3);
-
+        sleep(rand()%3);
         share->buffer[share->in] = rand() % 100;
-        printf("Produced item: %d at index: %d\n", share->buffer[share->in], share->in);
-        share->in = (share->in + 1) % bufferSize;
-
+        printf("item produced: %d in pos: %d\n", share->buffer[share->in], share->in);
+        share->in = (share->in+1)%bufferSize;
         sem_post(mutex);
         sem_post(full);
     }
 
     printf("[Producer] Process completed\n");
-
+    
     sem_close(full);
     sem_close(empty);
     sem_close(mutex);
     sem_unlink(semFullName);
     sem_unlink(semEmptyName);
     sem_unlink(semMutexName);
+    
     munmap(share, SIZE);
     close(fd);
     shm_unlink(NAME);
